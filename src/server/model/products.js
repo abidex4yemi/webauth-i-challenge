c528221964f3b5ab29004aaee07ef4708a862f1f
abidex4yemi <abidex4yemi@gmail.com>
2019-07-22 17:06:38 +0100

    fix: wrong product import model function

--- src/server/model/products.js
+++ src/server/model/products.js
@@ -1,7 +1,7 @@
-const productModels = require('../../db/helpers/product-model');
+const create = require('../../db/helpers/product-model');
 
 module.exports = (knex) => {
-  const models = productModels(knex);
+  const models = create(knex);
 
   return {
     ...models,
