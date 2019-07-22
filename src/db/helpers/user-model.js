30b346f4c3c54876f264fbbf10e2580a01796c7d
abidex4yemi <abidex4yemi@gmail.com>
2019-07-22 14:57:59 +0100

    Create users model helper function

---
+++ src/db/helpers/user-model.js
@@ -0,0 +1,46 @@
+/**
+ * users query builder
+ *
+ * @param {Object} knex
+ * @returns {Object} {get, insert, update, remove}
+ */
+export default (knex) => {
+  function getAll() {
+    return knex('users');
+  }
+
+  function getById(id) {
+    return knex('users')
+      .where({ id })
+      .first();
+  }
+
+  function insert(user) {
+    return knex('users')
+      .insert(user)
+      .then(([id]) => getById(id));
+  }
+
+  function update(id, changes) {
+    return knex('users')
+      .where({ id })
+      .update(changes)
+      .then(count => (count > 0 ? getById(id) : null));
+  }
+
+  function remove(id) {
+    return knex('users')
+      .where({ id })
+      .del();
+  }
+
+  // User is the model name
+  return {
+    name: 'User',
+    getById,
+    insert,
+    update,
+    remove,
+    getAll,
+  };
+};
